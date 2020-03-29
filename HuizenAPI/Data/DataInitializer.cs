0add9456545eab8d7150b56590e18b22056238e6
KilianHoefman <kilian.hoefman@student.hogent.be>
2020-03-30 01:42:20 +0200

    Seeding

--- HuizenAPI/Data/DataInitializer.cs
+++ HuizenAPI/Data/DataInitializer.cs
@@ -18,20 +18,37 @@ namespace HuizenAPI.Data
 
         public void InitializeData()
         {
-            //_dbContext.Database.EnsureDeleted();
+            _dbContext.Database.EnsureDeleted();
             if (_dbContext.Database.EnsureCreated())
             {
                 Locatie Gent1 = new Locatie("Gent", "Vlaanderenstraat", "1A", 9000);
-                _dbContext.Add(Gent1);
+                Locatie Gent2 = new Locatie("Gent", "Overpoortstraat", "6", 9000);
+                Locatie Merelbeke = new Locatie("Merelbeke", "Sint-elooistraat", "72", 9820);
+                Locatie[] locaties = new Locatie[] { Gent1, Gent2, Merelbeke };
+                _dbContext.AddRange(locaties);
 
                 Detail Detail1 = new Detail("Dit is een lange beschrijving", 200, 250, 200, 1400);
-                _dbContext.Add(Detail1);
+                Detail Detail2 = new Detail("Dit is een lange beschrijving v2", 400, 800, 733, 3466);
+                Detail Detail3 = new Detail("Dit is een lange beschrijving v3", 600, 1200, 560, 4500);
+                Detail[] details = new Detail[] { Detail1, Detail2, Detail3 };
+                _dbContext.AddRange(details);
 
-                ImmoBureau ImmoBureau1 = new ImmoBureau("Yolo");
-                _dbContext.Add(ImmoBureau1);
+                ImmoBureau Nobels = new ImmoBureau("Immo Nobels");
+                ImmoBureau DaVinci = new ImmoBureau("Immo Da Vinci");
+                ImmoBureau CD = new ImmoBureau("CD-Vastgoed");
+                ImmoBureau[] immoBureaus = new ImmoBureau[] { Nobels, DaVinci, CD };
+                
+                Huis Huis1 = new Huis(Gent1, "Dit is een korte beschrijving", 500000, Detail1, "koop", Nobels);
+                Huis Huis2 = new Huis(Gent2, "Dit is een korte beschrijving v2", 452000, Detail2, "koop", CD);
+                Huis Huis3 = new Huis(Merelbeke, "Dit is een korte beschrijving v3", 5000, Detail3, "huur", DaVinci);
+                Huis[] huizen = new Huis[] { Huis1, Huis2, Huis3 };
+                _dbContext.AddRange(huizen);
+                
+                Nobels.AddHuis(Huis1);
+                CD.AddHuis(Huis2);
+                DaVinci.AddHuis(Huis3);
+                _dbContext.AddRange(immoBureaus);
 
-                Huis Huis1 = new Huis(Gent1, "Dit is een korte beschrijving", 500000, Detail1, "koop", ImmoBureau1);
-                _dbContext.Add(Huis1);
             }
             _dbContext.SaveChanges();
         }
