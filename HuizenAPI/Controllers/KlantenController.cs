e28b886c567f21aa273361eb0d7b25be21753f5e
KilianHoefman <kilian.hoefman@student.hogent.be>
2020-03-30 01:24:38 +0200

    KlantenController toegevoegd

--- HuizenAPI/Controllers/KlantenController.cs
+++ HuizenAPI/Controllers/KlantenController.cs
@@ -14,12 +14,19 @@ namespace HuizenAPI.Controllers
     public class KlantenController : ControllerBase
     {
         private readonly IKlantRepository _klantenRepository;
-
         public KlantenController(IKlantRepository context)
         {
             _klantenRepository = context;
         }
 
+        /// <summary>
+        /// Geeft klanten adhv parameters
+        /// </summary>
+        /// <param name="voornaam">Voornaam van de klant als string</param>
+        /// <param name="achternaam">Achternaam van de klant als string</param>
+        /// <param name="email">Email van de klant als string</param>
+        /// <param name="telefoonNummer">Telefoonnummer van de klant als string</param>
+        /// <returns>Array van klanten</returns>
         [HttpGet]
         public IEnumerable<Klant> GetKlanten(string voornaam = null, string achternaam = null, string email = null, string telefoonNummer = null)
         {
@@ -28,6 +35,11 @@ namespace HuizenAPI.Controllers
             return _klantenRepository.GetBy(voornaam, achternaam, email, telefoonNummer);
         }
 
+        /// <summary>
+        /// Geeft klant terug met id
+        /// </summary>
+        /// <param name="id">Klantennummer van de gewenste klant</param>
+        /// <returns>Klant</returns>
         [HttpGet("{id}")]
         public ActionResult<Klant> GetKlant(int id)
         {
@@ -36,6 +48,12 @@ namespace HuizenAPI.Controllers
             return klant;
         }
 
+
+        /// <summary>
+        /// Voegt een nieuwe klant toe
+        /// </summary>
+        /// <param name="klantDTO">De nieuwe toe te voegen klant</param>
+        /// <returns>Gecreëerde Klant</returns>
         [HttpPost]
         public ActionResult<Klant> PostKlant(KlantDTO klantDTO)
         {
@@ -46,6 +64,11 @@ namespace HuizenAPI.Controllers
             return CreatedAtAction(nameof(GetKlant), new { id = klantToCreate.KlantenNummer }, klantToCreate);
         }
 
+        /// <summary>
+        /// Wijzigt een Klant
+        /// </summary>
+        /// <param name="id">Klantennummer van de te wijzigen klant</param>
+        /// <param name="klant">Gewijzigde klant</param>        
         [HttpPut("{id}")]
         public IActionResult PutKlant(int id, Klant klant)
         {
@@ -58,6 +81,10 @@ namespace HuizenAPI.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Verwijdert een klant met id
+        /// </summary>
+        /// <param name="id">Klantennummer van de te verwijderen klant</param>        
         [HttpDelete("{id}")]
         public IActionResult DeleteKlant(int id)
         {
